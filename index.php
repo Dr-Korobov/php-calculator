eec8c287aea39c0459b372fc705abd8a1805c76d
Danil Korobov <dkorobov555@gmail.com>
2020-04-06 20:52:23 +0300

    Easy calculator

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+    <h2>Calculator </h2>
+        <h5><?php echo $_GET[a], ' + ',$_GET[b], ' = ', $_GET[a]+$_GET[b] ?> </h5>
+        <h5><?php echo $_GET[a], ' - ',$_GET[b], ' = ', $_GET[a]-$_GET[b] ?> </h5>
+        <h5><?php echo $_GET[a], ' * ',$_GET[b], ' = ', $_GET[a]*$_GET[b] ?> </h5>
+        <h5><?php echo $_GET[a], ' / ',$_GET[b], ' = ', $_GET[a]/$_GET[b] ?> </h5>
+    
+</body>
+<html>
+
