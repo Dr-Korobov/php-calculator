562e835eb82c98be6d457a7a1e36e8f300a928ea
Danil Korobov <dkorobov555@gmail.com>
2020-04-14 15:13:52 +0300

    Long check Calc

--- index.php
+++ index.php
@@ -1,12 +1,47 @@
+  
 <html>
 <head></head>
 <body>
-    <h2>Calculator </h2>
-        <h5><?php echo $_GET[a], ' + ',$_GET[b], ' = ', $_GET[a]+$_GET[b] ?> </h5>
-        <h5><?php echo $_GET[a], ' - ',$_GET[b], ' = ', $_GET[a]-$_GET[b] ?> </h5>
-        <h5><?php echo $_GET[a], ' * ',$_GET[b], ' = ', $_GET[a]*$_GET[b] ?> </h5>
-        <h5><?php echo $_GET[a], ' / ',$_GET[b], ' = ', $_GET[a]/$_GET[b] ?> </h5>
-    
+    <h1>
+        Calculator
+    </h1>
+    <p>
+    <?php
+       
+        $operation = $_GET['o'];
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+
+        if ($operation != 'sum' && $operation != 'sub' && $operaion != 'div' && $operation != 'mul') {
+            echo 'Unexpected operation';
+            exit;
+        }
+
+        if (!is_numeric($a) || !is_numeric($b)) {
+           echo 'Invalid arguments';
+           exit;
+        }
+
+        if ($operation == 'sum') {
+            $result = $a + $b;
+        }
+
+        if ($operation == 'sub') {
+            $result = $a - $b;
+        }
+
+        if ($operation == 'div') {
+            $result = $a / $b;
+        }
+
+        if ($operation == 'mul') {
+            $result = $a * $b;
+        }
+
+       
+        echo $operation, ' is ', $result;
+    ?>
+    </p>
 </body>
-<html>
+</html>
 
