7530bc2694e58452d424567fd07086f99269ceab
Danil Korobov <dkorobov555@gmail.com>
2020-04-27 19:09:36 +0300

    Calc(POST-)

--- index.php
+++ index.php
@@ -7,16 +7,39 @@
     <p>
     <?php
        
-        $operation = $_GET['o'];
-        $a = $_GET['a'];
-        $b = $_GET['b'];
+        $operation = isset($_POST['o'])? $_POST['o']: 'undefined';
+        $a = (int)$_POST['a'];
+        $b = (int)$_POST['b'];
         $result = null;
 
         if (!is_numeric($a) || !is_numeric($b)) {
            echo 'Invalid arguments';
            exit;
         }
+    ?>
+    <form method= "POST">
+        <div>
+             <input type="text" name= "a" placeholder= "Enter the first number"
+        </div>
+        <div>
+             <input type="text" name= "b" placeholder= "Enter the second number"
+        </div>
+        <div>
+        <label>Operation</label>
+        <select name="operation">
+            <option value="sum">Sum</option>
+            <option value="sub">Subtraction</option>
+            <option value="mul">Multiply</option>
+            <option value="div">Divide</option>
+        </select>
+    </div>
+    <br>
+    <div>
+        <button type="submit">Calculate</button>
+    </div>
+  </form>
 
+     <?php
         if ($operation === 'sum') {
             $result = $a + $b;
         }
@@ -33,11 +56,11 @@
             $result = $a * $b;
         }
 
-        if ($result === null) {
+        /*if ($result === null) {
             echo 'Unexpected operation';
             exit;
         }
-       
+       */
         echo $operation, ' is ', $result;
     ?>
     </p>
